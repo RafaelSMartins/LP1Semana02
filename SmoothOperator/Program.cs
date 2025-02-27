f700ef00525a204d2ac523772c47c48b08f8434c
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-27 23:29:38 +0000

    Smooth Opereator add

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("insere número inteiro:");
+            sbyte n= sbyte.Parse(Console.ReadLine());
+
+            --n;
+            Console.WriteLine(n);
+            ++n;
+            Console.WriteLine(n);
+
+        }
+    }
+}
