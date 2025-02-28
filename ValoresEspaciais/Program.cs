ba972c0e2056ecced1b39b9145129bc61ac5a344
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-28 09:14:41 +0000

    max min values

---
+++ ValoresEspaciais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ValoresEspaciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(ushort.MinValue);
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(ulong.MaxValue);
+        }
+    }
+}
