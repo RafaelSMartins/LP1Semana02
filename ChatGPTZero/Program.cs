df075b08ea66e3d34d9a95d90ea95b02055d6484
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-28 13:27:39 +0000

    chat gpt zero primeira pergunta e resposta

---
+++ ChatGPTZero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChatGPTZero
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Are you more intelligent than me? ");
+            string question = Console.ReadLine();
+
+            string response = "Obviously!";
+
+            Console.WriteLine(response);
+        }
+    }
+}
+
